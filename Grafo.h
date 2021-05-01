daff7d3189f3b54049469dc95467e654ba3ce43a
Vitor Bonella <vitorb.bonella36@gmail.com>
2021-05-01 15:45:21 -0300

    Changes by danilo

--- Grafo.h
+++ Grafo.h
@@ -7,11 +7,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "pagina.h"
+#include "tabelaHash.h"
 #include "Lista.h"
 
 typedef struct grafo Grafo;
 
 Grafo* inicializaGrafo();
+Grafo* preencheGrafo(Grafo * g, Hash * paginas,int qtdPag);
 void setVertices(Grafo*g,Lista* lista);
 
 #endif //TRABALHO_3_DE_ED2_GRAFO_H
